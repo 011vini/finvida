5bf356a5f33c0b0f5f246f440328c653dab8b3f8
Vinicio Cauã <vinicio.caua.silava@gmail.com>
2026-05-12 12:28:03 -0300

    .

--- backend/config/conexao.php
+++ backend/config/conexao.php
@@ -1,15 +1,17 @@
 <?php
 
-$host = "localhost";
+$host = "127.0.0.1";
 $usuario = "root";
 $senha = "";
-$banco = "orcamente";
+$banco = "finvida";
+$porta = 3307;
 
 $conn = new mysqli(
     $host,
     $usuario,
     $senha,
-    $banco
+    $banco,
+    $porta
 );
 
 if($conn->connect_error){
@@ -17,6 +19,5 @@ if($conn->connect_error){
     die("Erro: " . $conn->connect_error);
 }
 
-$conn->set_charset("utf8");
-
+echo "Banco conectado";
 ?>
